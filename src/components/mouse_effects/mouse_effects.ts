ac0982c13fe3bfe932d7723ab45250d7f17d838b
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-07-03 10:28:34 -0400

    keep ellipsis after click

--- src/components/mouse_effects/mouse_effects.ts
+++ src/components/mouse_effects/mouse_effects.ts
@@ -5,31 +5,38 @@ import { Component } from "../../component";
 import './mouse_effects.css';
 
 export class MouseEffects extends Component<any> {
-    mouse_click_ellipsis: HTMLDivElement;
-
     constructor(app: App) {
         super(app, app.root, `mouse-effects`);
-
-        this.mouse_click_ellipsis = document.createElement(`div`);
-        this.mouse_click_ellipsis.classList.add(`mouse-effects-click`);
-        this.element.appendChild(this.mouse_click_ellipsis);
     }
 
-    show(): void {
-        super.show();
+    on_click = (e: MouseEvent) => {
+        const mouse_click_ellipsis = document.createElement(`div`);
+        mouse_click_ellipsis.classList.add(`mouse-effects-click`);
+        this.element.appendChild(mouse_click_ellipsis);
 
-        this.app.root.addEventListener(`click`, (e: MouseEvent) => {
-            this.mouse_click_ellipsis.style.translate = `calc(${e.clientX}px - 2.5rem) calc(${e.clientY}px - 2.5rem)`;
-            animate(this.mouse_click_ellipsis, {
-                opacity: [0, 0.5, 0],
-                scale: [0, 1.5],
-                duration: 500,
-                ease: eases.linear(),
-            });
-
-            const click_audio = new Audio(`/audio/sound_effects/mouse_click_1.mp3`);
-            click_audio.volume = 1;
-            this.app.audio.play_audio(`sound_effect`, click_audio);
+        mouse_click_ellipsis.style.translate = `calc(${e.clientX}px - 2.5rem) calc(${e.clientY}px - 2.5rem)`;
+        animate(mouse_click_ellipsis, {
+            opacity: [0, 0.5, 0],
+            scale: [0, 1.5],
+            duration: 500,
+            ease: eases.linear(),
+            onComplete: () => {
+                mouse_click_ellipsis.remove();
+            }
         });
+
+        const click_audio = new Audio(`/audio/sound_effects/mouse_click_1.mp3`);
+        click_audio.volume = 1;
+        this.app.audio.play_audio(`sound_effect`, click_audio);
+    }
+
+    show() {
+        super.show();
+        this.app.root.addEventListener(`click`, this.on_click);
+    }
+
+    hide() {
+        super.hide();
+        this.app.root.removeEventListener(`click`, this.on_click);
     }
 }
