e7f189298bde413c19bf0b098698480c453db134
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-07-07 12:40:05 -0400

    new video component

---
+++ src/pages/quest/video/video.ts
@@ -0,0 +1,59 @@
+import { animate, eases } from "animejs";
+import { App } from "../../../app";
+import { Component } from "../../../component";
+
+import './video.css';
+
+export interface VideoProps {
+    src: string;
+}
+
+export class Video extends Component<any> {
+    video_element: HTMLVideoElement;
+    btn_continue: HTMLButtonElement;
+
+    constructor(app: App, parent: HTMLElement) {
+        super(app, parent, `quest-video`);
+
+        this.video_element = document.createElement(`video`);
+        this.video_element.classList.add(`quest-video-canvas`);
+        this.video_element.controls = true;
+        this.element.appendChild(this.video_element);
+
+        this.btn_continue = document.createElement(`button`);
+        this.btn_continue.classList.add(`quest-video-btn-continue`);
+        this.btn_continue.innerHTML = `CONTINUE`;
+        this.btn_continue.addEventListener(`click`, () => {
+            this.leave();
+        });
+
+        this.element.appendChild(this.btn_continue);
+    }
+
+    appear(props: VideoProps) {
+        this.load();
+
+        this.video_element.src = props.src;
+        this.video_element.muted = false;
+        this.video_element.autoplay = true;
+
+        animate(this.element, {
+            opacity: [.5, 1],
+            scale: [1.3, 1],
+            duration: 500,
+            ease: eases.inOutBack(2)
+        });
+    }
+
+    leave() {
+        animate(this.element, {
+            opacity: [1, 0],
+            translateY: [0, `100%`],
+            duration: 500,
+            onComplete: () => {
+                this.unload();
+                this.app.quest_page.forward();
+            }
+        });
+    }
+}
